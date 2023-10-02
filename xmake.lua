2807cd1639df4b6a3daa23e1e13f9db7390c467b
jun <dknightjun@gmail.com>
2023-10-03 00:25:19 +0800

    macos编译libpng

--- xmake.lua
+++ xmake.lua
@@ -1,65 +1,92 @@
 package("libapng")
-
-    set_homepage("https://wiki.mozilla.org/APNG_Specification")
-    set_description("The official PNG reference library with apng patch")
-
-    set_sourcedir(path.join(os.scriptdir(), "./"))
+    set_homepage("http://www.libpng.org/pub/png/libpng.html")
+    set_description("The official PNG reference library")
     set_license("libpng-2.0")
 
+    add_urls("https://github.com/glennrp/libpng/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://mirrors.ustc.edu.cn/debian/pool/main/libp/libpng1.6/libpng1.6_$(version).orig.tar.gz", {
+        version = function (version) return version:sub(2) end
+    })
+    add_urls("https://github.com/glennrp/libpng.git")
+
+    add_versions("v1.6.40", "62d25af25e636454b005c93cae51ddcd5383c40fa14aa3dae8f6576feb5692c2")
+
     add_deps("zlib")
-    if is_host("windows") then
-        add_deps("cmake")
+
+    if is_plat("linux") then
+        add_syslinks("m")
     end
 
-    on_install("windows", function (package)
-        local configs = {"-DPNG_TESTS=OFF",
-                         "-DPNG_SHARED=" .. (package:config("shared") and "ON" or "OFF"),
-                         "-DPNG_STATIC=" .. (package:config("shared") and "OFF" or "ON"),
-                         "-DPNG_DEBUG=" .. (package:debug() and "ON" or "OFF")}
-        import("package.tools.cmake").install(package, configs)
-    end)
 
-    on_install("mingw@msys", function (package)
-        local mkFile = "scripts/makefile.gcc"
-        local zlib = package:dep("zlib")
-        io.gsub(mkFile, "\nZLIBINC =.-\n",      "\nZLIBINC=" .. (os.args(path.join(zlib:installdir(), "include"))) .. "\n")
-        io.gsub(mkFile, "\nZLIBLIB =.-\n",      "\nZLIBLIB=" .. (os.args(path.join(zlib:installdir(), "lib"))) .. "\n")
-        import("package.tools.make").build(package, {"-f", "scripts/makefile.gcc", "libpng.a"})
-        os.cp("libpng.a", package:installdir("lib"))
-        os.cp("*.h", package:installdir("include"))
+    on_install("windows", "mingw", "android", "iphoneos", "cross", "bsd", "wasm", function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            add_requires("zlib")
+            target("png")
+                set_kind("$(kind)")
+                add_files("*.c|example.c|pngtest.c")
+                if is_arch("x86", "x64", "i386", "x86_64") then
+                    add_files("intel/*.c")
+                    add_defines("PNG_INTEL_SSE_OPT=1")
+                    add_vectorexts("sse", "sse2")
+                elseif is_arch("arm.*") then
+                    add_files("arm/*.c")
+                    if is_plat("windows") then
+                        add_defines("PNG_ARM_NEON_OPT=1")
+                        add_defines("PNG_ARM_NEON_IMPLEMENTATION=1")
+                    else
+                        add_files("arm/*.S")
+                        add_defines("PNG_ARM_NEON_OPT=2")
+                    end
+                elseif is_arch("mips.*") then
+                    add_files("mips/*.c")
+                    add_defines("PNG_MIPS_MSA_OPT=2")
+                elseif is_arch("ppc.*") then
+                    add_files("powerpc/*.c")
+                    add_defines("PNG_POWERPC_VSX_OPT=2")
+                end
+                add_headerfiles("*.h")
+                add_packages("zlib")
+                if is_kind("shared") and is_plat("windows") then
+                    add_defines("PNG_BUILD_DLL")
+                end
+        ]])
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        elseif not package:is_plat("windows", "mingw") and package:config("pic") ~= false then
+            configs.cxflags = "-fPIC"
+        end
+        if package:is_plat("android") and package:is_arch("armeabi-v7a") then
+            io.replace("arm/filter_neon.S", ".func", ".hidden", {plain = true})
+            io.replace("arm/filter_neon.S", ".endfunc", "", {plain = true})
+        end
+        os.cp("scripts/pnglibconf.h.prebuilt", "pnglibconf.h")
+        import("package.tools.xmake").install(package, configs)
     end)
 
     on_install("macosx", "linux", function (package)
-        local configs = {"--disable-dependency-tracking", "--disable-silent-rules"}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
-            table.insert(configs, "--enable-static=no")
-        else
-            table.insert(configs, "--enable-static=yes")
-            table.insert(configs, "--enable-shared=no")
+        local configs = {}
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        table.insert(configs, "--enable-static=" .. (package:config("shared") and "no" or "yes"))
+        if package:config("pic") ~= false then
+            table.insert(configs, "--with-pic")
         end
-        import("package.tools.autoconf").install(package, configs)
-    end)
-
-    on_install("iphoneos", "android@linux,macosx", function (package)
-        import("package.tools.autoconf")
-        local zlib = package:dep("zlib")
-        local envs = autoconf.buildenvs(package)
-        if zlib then
-            -- we need patch cflags to cppflags for supporting zlib on android ndk
-            -- @see https://github.com/xmake-io/xmake/issues/1126
-            envs.CPPFLAGS = (envs.CFLAGS or "") .. " -I" .. os.args(path.join(zlib:installdir(), "include"))
-            envs.LDFLAGS = (envs.LDFLAGS or "") .. " -L" .. os.args(path.join(zlib:installdir(), "lib"))
-        end
-        local configs = {"--disable-dependency-tracking", "--disable-silent-rules"}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
-            table.insert(configs, "--enable-static=no")
-        else
-            table.insert(configs, "--enable-static=yes")
-            table.insert(configs, "--enable-shared=no")
-        end
-        autoconf.install(package, configs, {envs = envs})
+        local cppflags = {}
+        local ldflags = {}
+        for _, dep in ipairs(package:orderdeps()) do
+            local fetchinfo = dep:fetch()
+            if fetchinfo then
+                for _, includedir in ipairs(fetchinfo.includedirs or fetchinfo.sysincludedirs) do
+                    table.insert(cppflags, "-I" .. includedir)
+                end
+                for _, linkdir in ipairs(fetchinfo.linkdirs) do
+                    table.insert(ldflags, "-L" .. linkdir)
+                end
+            end
+        end    
+        table.insert(cppflags, "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk")
+        import("package.tools.autoconf").install(package, configs, {cppflags = cppflags, ldflags = ldflags})
     end)
 
     on_test(function (package)
